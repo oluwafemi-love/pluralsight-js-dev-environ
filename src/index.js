372cd72fb9f575b50f38b19c48201ea67dbd112e
oluwafemi-love <42425031+oluwafemi-love@users.noreply.github.com>
2019-08-01 02:24:59 +0100

    A work in progress - Many concept to grasp yo! - We getting there, Ine!

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*eslint-disable no-console */
+/* eslint-disable no-debugger*/
+import './index.css';
+import numeral from 'numeral';
+
+const courseValue =numeral(1000).format('$0,0.00');
+debugger;
+console.log(`I would pay ${courseValue} for this awesome course!`);
